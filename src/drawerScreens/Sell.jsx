bdc48ba09b77e67bc59594c8f21c6bfba95b8d8b
kbht980 <actor.kubo@gmail.com>
2021-03-17 22:06:49 +0900

    ドロワーメニューファイル生成

---
+++ src/drawerScreens/Sell.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export default function Sell(){
+    return(
+        <View>
+            <Text>NeighborFarmで販売する</Text>
+        </View>
+    )
+}
