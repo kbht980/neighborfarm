f5fc5f5609142e4d00b2d4b5b170cefa58149f08
kbht980 <actor.kubo@gmail.com>
2021-03-17 21:48:56 +0900

    ドロワーファイル追加

---
+++ src/drawerScreens/Payment.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View , Text} from  'react-native';
+
+export default function Payment(){
+    return(
+        <View>
+            <Text>
+                お支払い方法
+            </Text>
+        </View>
+    )
+}
